1804b59cf7616359f2d2e593296ea589ab1eee5d
Doan Ngoc Triet <trietdoanngoc@gmail.com>
2026-05-30 11:31:29 +0700

    Update AboutUs.jsx

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -5,7 +5,6 @@ function AboutUs() {
     <div className="about-us-container">
       <h2>About Paradise Nursery</h2>
       <p>Welcome to Paradise Nursery, where green meets serenity. Our mission is to bring life, freshness, and aesthetics into your living and working spaces by providing high-quality, sustainably grown houseplants.</p>
-      <p>Whether you are a seasoned plant parent or just starting your green journey, we are here to guide you every step of the way.</p>
     </div>
   );
 }
